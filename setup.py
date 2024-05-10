f84b1d3d7a3bb44407f1758bcb95aa8b39c9dafb
maharengarajan <maharengarajan46@gmail.com>
2024-05-10 18:25:22 +0530

    req installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Renga Rajan K",
+    author_email="maharengarajan46@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
